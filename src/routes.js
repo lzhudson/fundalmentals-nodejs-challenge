32b8bd90fa1057aa51b832173e97899aaf8fbffb
Hudson Holanda <hudsonholanda55@gmail.com>
2024-06-08 10:21:52 -0300

    feat: implement route to list all tasks

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,14 @@ const database = new Database();
 
 export const routes = [
   {
+    method: "GET",
+    path: "/tasks",
+    handler: (req, res) => {
+      const tasks = database.select("tasks");
+      return res.end(JSON.stringify(tasks))
+    },
+  },
+  {
     method: "POST",
     path: "/tasks",
     handler: (req, res) => {
