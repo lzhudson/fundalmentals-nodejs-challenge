c07c61eaff113373c3d58c47727872e4890139a0
Hudson Holanda <hudsonholanda55@gmail.com>
2024-06-08 10:18:39 -0300

    feat: implement handler handler route

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
 import http from 'node:http';
+import { json } from './middlewares/json.js';
+import { routes } from './routes.js';
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
 
+  await json(req, res);
+  console.log(method, url);
+  const route = routes.find(route => {
+    return route.method === method && route.path === url;
+  })
+
+  if(route) {    
+    return route.handler(req, res);
+  }
 })
 
 server.listen(3333);
